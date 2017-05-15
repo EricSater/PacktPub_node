ed0d4538e5bd05ba04ce97b34507f90bc00d6882
Eric Sater <EricSater@users.noreply.github.com>
2017-05-14 18:14:20 -0600

    Update script.js
    Add comment about file types.

--- script.js
+++ script.js
@@ -7,12 +7,13 @@ const password = ""; // Enter your password here
 // String.raw is a verbatim string which allows you to put in a file path without needing to escape the slashes.
 const rootDirectory = String.raw``;
 
+// Set these variables to false if you do not want that file type to be downloaded.
 const pdf = true;
 const ePub = true;
 const mobi = true;
-// I don't know how the Kindle download works because I don't have a Kindle to try it on.
-// const kindle = true;
-const code = true;
+// const kindle = true; // I don't know how the Kindle download works because I don't have a Kindle to try it on.
+const code = true; // A zip folder with code examples
+
 const packtApi = new PacktApi(pdf, ePub, mobi, code);
 
 (async () => {
